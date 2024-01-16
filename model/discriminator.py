7cca059b5342da31f873bb4937c52400e832a28b
Giulia Di Fede <giuliadifede1102@gmail.com>
2024-01-16 09:43:06 +0100

    trying to get discriminator checkpoints

--- model/discriminator.py
+++ model/discriminator.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
 
 class FCDiscriminator(nn.Module):
 
-	def __init__(self, num_classes, ndf = 64):
+	def __init__(self, num_classes, ndf = 64, pretrain_model=''):
 		super(FCDiscriminator, self).__init__()
 
 		self.conv1 = nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1)
@@ -16,6 +16,10 @@ class FCDiscriminator(nn.Module):
 		self.leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 		#self.up_sample = nn.Upsample(scale_factor=32, mode='bilinear')
 		#self.sigmoid = nn.Sigmoid()
+
+		if pretrain_model:
+			print('use pretrain model {}'.format(pretrain_model))
+			self.init_weight(pretrain_model)
 
 
 	def forward(self, x):
@@ -32,3 +36,10 @@ class FCDiscriminator(nn.Module):
 		#x = self.sigmoid(x) 
 
 		return x
+	
+	def _init_weight(self, pretrain_model):
+		state_dict = torch.load(pretrain_model)["state_dict"]
+		self_state_dict = self.state_dict()
+		for k, v in state_dict.items():
+			self_state_dict.update({k: v})
+		self.load_state_dict(self_state_dict)
